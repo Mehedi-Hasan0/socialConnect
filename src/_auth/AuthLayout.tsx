31148e32f209a3724cb3fe287778f2d192972a12
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-11-25 22:57:03 +0600

    Necessary dependency added and authlayout routes added

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,21 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>Auth layout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to={"/"} />
+      ) : (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const SignupForm = () => {
-  return <div>Sign up form</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default SignupForm;
