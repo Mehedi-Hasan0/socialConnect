4c1ccf3931baa3509f32cb64bd8196c77c6ee6a1
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-12-14 18:13:32 +0600

    Feat: create posts api added

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,18 @@
+import Loader from "@/components/shared/Loader";
+
 const Home = () => {
-  return <div>Home</div>;
+  const isPostLoading = true;
+  const posts = null;
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? <Loader /> : <ul></ul>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
